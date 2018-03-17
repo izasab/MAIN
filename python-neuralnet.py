287347c2c6ab57cb1d21f0e784848fd07c9b3c82
l_wl <nic@bigo.ooo>
2018-03-17 18:07:08 -0400

    INIT
    INIT

---
+++ python-neuralnet.py
@@ -0,0 +1,25 @@
+
+from __future__ import print_function
+
+
+
+def lewl():
+	l=1
+	e='_'
+	w=chr(ord('w'))
+
+	lol=[l,e,w,l]
+	
+	for i in lol:
+
+		print(str(i), end = '')
+
+	print()
+	print(lewl)
+
+
+
+lewl()
+
+
+lewl()
