b6f229527804a0894477cc23d8da0ea542d1799b
l_wl <nic@bigo.ooo>
2018-03-18 00:20:04 -0400

    do
    do

--- ns-net.py
+++ ns-net.py
@@ -20,20 +20,29 @@ random.seed(1)
 
 weights = 2 * random.random((3, 1)) - 1
 
+print inputs
 
-for i in xrange(10000):
+for i in xrange(100):
+	print i
 	
 	out = sig(dot(inputs, weights))
+	print 'out: \n', out
+
 
 	error = outputs - out
+	print 'error: \n', error
+
 
 	adj = error * sig_der(out)
-
+	print 'adj: \n', adj
 
 
 	weights += dot(inputs.T, adj)
 
+	print
 
+	print weights
+	
 	#print weights
 	
 
